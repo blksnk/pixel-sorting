0c97bddc85a12613cf761897829a673276b61184
JN Veigel <art.jnveigel@gmail.com>
2021-04-04 00:46:52 +0200

    :sparkles: :construction: color mask

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import PixelSort from './lib/pixelSort.js';
+import { hexCodeToPixel } from './lib/utils.js';
 
 const options = {
   direction: 'vertical',
   url:
-    'https://images.unsplash.com/photo-1509114397022-ed747cca3f65?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-  sortBy: 'sum',
+    'https://images.unsplash.com/photo-1617467631235-1bf15aa7072c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80',
+  sortBy: 'mul',
   zones: true,
-  zoneThreshold: {
-    x: 20,
-    y: 10,
+  zoneThreshold: 90,
+  invertDirection: false,
+  colorMask: {
+    //color to search for
+    color: 'ffffff',
+    // variation coef for hue, saturation and brightness
+    variation: 2,
+    enabled: true,
   },
-  invertDirection: true,
 };
 
 const ps = new PixelSort(options);
+console.log(ps.colorMask);
